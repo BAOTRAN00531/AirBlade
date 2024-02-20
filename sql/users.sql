aee13bebe51f21b8f9c9e541c49205c8c9c4f76a
Andrew0456 <115640972+Andrew0456@users.noreply.github.com>
2024-02-20 18:49:48 +0700

    Delete sql directory

--- sql/users.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `users` (
-  `Id` int(100) NOT NULL,
-  `Username` varchar(200) DEFAULT NULL,
-  `Email` varchar(200) DEFAULT NULL,
-  `Password` varchar(200) DEFAULT NULL,
-  `Address` varchar(200) NOT NULL,
-  `Phone` varchar(10) NOT NULL
-)
