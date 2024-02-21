43a504257d88d063da6304d78794920a0d107993
khoa20060702 <149952778+khoa20060702@users.noreply.github.com>
2024-02-21 13:06:07 +0700

    added regex pattern
    need further improvements

--- muahang.php
+++ muahang.php
@@ -52,26 +52,26 @@
                         <h3>Địa chỉ thanh toán</h3>
                         <form action="hoadon.php" method="post">
                             <label for="HOTEN">Họ và Tên: </label><br>
-                            <input type="text" id="HOTEN" name="HOTEN"
+                            <input type="text" pattern="^sáàảãạăắằẳẵặâấầẩẫậéèẻẽẹêếềểễệíìỉĩịóòỏõọôốồổỗộơớờởỡợúùủũụưứừửữựýỳỷỹỵÁÀẢÃẠĂẮẰẲẴẶÂẤẦẨẪẬÉÈẺẼẸÊẾỀỂỄỆÍÌỈĨỊÓÒỎÕỌÔỐỒỔỖỘƠỚỜỞỠỢÚÙỦŨỤƯỨỪỬỮỰÝỲỶỸỴđĐ]+$/" id="HOTEN" name="HOTEN" required
                                 placeholder="Nhập họ và tên.."><br>
                             <label for="email">Email: </label><br>
-                            <input type="email" id="email" name="email"
+                            <input type="email" id="email" name="email" required
                                 placeholder="Nhập email.." ><br>
                             <label for="phone">Số điện thoại: </label><br>
-                            <input type="SDT" id="SDT" name="SDT" placeholder="Nhập số điện thoại" >
+                            <input type="SDT" id="SDT" name="SDT" pattern="^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$" placeholder="Nhập số điện thoại" required >
                             <br>
                             <label for="DIACHI">Địa chỉ nhận hàng: </label> <br>
-                            <input type="text" id="DIACHI" name="DIACHI"
+                            <input type="text" pattern="^sáàảãạăắằẳẵặâấầẩẫậéèẻẽẹêếềểễệíìỉĩịóòỏõọôốồổỗộơớờởỡợúùủũụưứừửữựýỳỷỹỵÁÀẢÃẠĂẮẰẲẴẶÂẤẦẨẪẬÉÈẺẼẸÊẾỀỂỄỆÍÌỈĨỊÓÒỎÕỌÔỐỒỔỖỘƠỚỜỞỠỢÚÙỦŨỤƯỨỪỬỮỰÝỲỶỸỴđĐ]+$/" id="DIACHI" name="DIACHI" required
                                 placeholder="Nhập địa chỉ.." > <br>
                                 <input type="hidden" id="iduser" name="iduser" value="11">
                                 <input type="hidden" id="idsp" name="idsp" value="22">
                                 <input type="hidden" name="TONG" id="TONG" value="100000">
 
-                                    <input class="item1" type="radio" id="cod" name="pttt" value="<?=1?>">
+                                    <input class="item1" type="radio" id="cod" name="pttt" value="<?=1?>" required>
                                     <label class="item2" for="cod">COD</label>
-                                    <input class="item1" type="radio" id="momo" name="pttt" value="<?=2?>">
+                                    <input class="item1" type="radio" id="momo" name="pttt" value="<?=2?>" required>
                                     <label class="item2" for="momo">momo</label>
-                                    <input class="item1" type="radio" id="vnpay" name="pttt" value="<?=3?>">
+                                    <input class="item1" type="radio" id="vnpay" name="pttt" value="<?=3?>" required>
                                     <label class="item2" for="vnpay">vnpay</label>
                       
                             <table class="chuyen-trang"><br>
