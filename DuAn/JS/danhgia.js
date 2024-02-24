75436107779152c26683f0fbb502c7b5faa82a47
CammyUyen <149951846+PhuongUyn20@users.noreply.github.com>
2024-02-24 16:47:51 +0700

    font-end
    hố la

---
+++ DuAn/JS/danhgia.js
@@ -0,0 +1,23 @@
+const allStar = document.querySelectorAll('.rating .star')
+const ratingValue = document.querySelector('.rating input')
+
+allStar.forEach((item, idx)=> {
+	item.addEventListener('click', function () {
+		let click = 0
+		ratingValue.value = idx + 1
+
+		allStar.forEach(i=> {
+			i.classList.replace('bxs-star1', 'bx-star1')
+			i.classList.remove('active')
+		})
+		for(let i=0; i<allStar.length; i++) {
+			if(i <= idx) {
+				allStar[i].classList.replace('bx-star1', 'bxs-star1')
+				allStar[i].classList.add('active')
+			} else {
+				allStar[i].style.setProperty('--i', click)
+				click++
+			}
+		}
+	})
+})
