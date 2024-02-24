1a035d08f7dfae291d4622a98ed98f4d0e19bb0f
BAOTRAN00531 <150698874+BAOTRAN00531@users.noreply.github.com>
2024-02-24 23:08:02 +0700

    update_3

---
+++ view/content/product.php
@@ -0,0 +1,9 @@
+<div class="row">
+            <!-- Dãy hiển thị sản phẩm 1 -->
+                <div class="product">
+                    <img src="../IMG/Products1.jpg" alt="Sản phẩm 1">
+                    <h4>Tên sản phẩm 1</h4>
+                    <p>Giá: 100.000 VND</p>
+                    <button>Mua ngay</button>
+                </div>
+            </div>
