66f5f7fd0f0e76aa9aab803c9bf01eedbf084791
khoa20060702 <149952778+khoa20060702@users.noreply.github.com>
2024-02-24 23:11:14 +0700

    added baidang

---
+++ view/post/baidang_process.php
@@ -0,0 +1,16 @@
+<?php 
+foreach ( $list_baidang as $baidang )
+{
+    extract( $baidang );
+    echo $NoiDung;
+    echo "<br>";
+    echo $Tag;
+    echo "<br>";
+    echo $UserID;
+    echo "<br>";
+    echo $Image;
+    echo "<br>";
+    echo $NgayDang;
+}
+?>
+
