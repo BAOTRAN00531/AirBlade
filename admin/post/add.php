194b7a14d42013d0243c5cc98177b21edac62f33
BAOTRAN00531 <150698874+BAOTRAN00531@users.noreply.github.com>
2024-02-28 19:39:45 +0700

    fix post

--- admin/post/add.php
+++ admin/post/add.php
@@ -10,7 +10,7 @@
     <h1>THÊM BÀI VIẾT</h1>
         <form action="index.php?action=addpost" id="post-form" method="post" >
         <div class="">Tên: <br>
-                <input type="text" name="ten" id="rowlist" > 
+                <input type="text" name="tenbv" id="rowlist" > 
         </div>
         <br>
         <div class="">Hình: <br>
@@ -36,7 +36,7 @@
             </select>
         </div>
             <div>
-                <input type="submit" id="add" name="addlist" value="Thêm">
+                <input type="submit" id="add" name="addpost" value="Thêm">
                 <input type="reset" value="Nhập lại">
                 <a href="index.php?action=listdm"><input type="button" value="Danh Sách">
             </div>
