5b0d093e277e37ac5b9322258a36884b1c43f9d8
khoa20060702 <149952778+khoa20060702@users.noreply.github.com>
2024-02-28 16:51:26 +0700

    updated catalogs

--- view/product/catalogs.php
+++ view/product/catalogs.php
@@ -7,15 +7,39 @@
         foreach( $list1 as $first )
         {
             extract($first);
+            $hinh = $first['hinhanh'];
+            $MASP = $first['MASP'];
+            $TENSP = $first['TENSP'];
+            $GIASP = $first['GIASP'];
+            echo "
+            <div class='product'>
+            <a href='index.php?action=showdetail&idsp=$MASP'><img height='100px' width='100px' src='../uploads/$hinh' alt='Sản phẩm 1'></a>
+             <h4>$TENSP</h4>
+             <p>Giá: $GIASP VND</p>
+             <button>Mua ngay</button>
+         </div>";
         }
-        echo $first['IDDM'];
         ?>
         </div>
 
         <h2>HONDA</h2>
         <div class="row"> 
         <?php 
-        echo $iddm2;
+        foreach( $list2 as $second )
+        {
+            extract($second);
+            $hinh = $second['hinhanh'];
+            $MASP = $second['MASP'];
+            $TENSP = $second['TENSP'];
+            $GIASP = $second['GIASP'];
+            echo "
+            <div class='product'>
+            <a href='index.php?action=showdetail&idsp=$MASP'><img height='100px' width='100px' src='../uploads/$hinh' alt='Sản phẩm 1'></a>
+             <h4>$TENSP</h4>
+             <p>Giá: $GIASP VND</p>
+             <button>Mua ngay</button>
+         </div>";
+        }        
         ?>  
         </div>
     </div>
