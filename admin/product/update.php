c0e1e7d0ca8ff9e79788f3cfd9768e9f2299c388
Andrew0456 <phandat452006@gmail.com>
2024-02-29 21:37:41 +0700

    Hi

--- admin/product/update.php
+++ admin/product/update.php
@@ -7,24 +7,26 @@ if (is_array($listdm)) {
 }
 $imgPATH = "../uploads/" . $hinhanh;
 if (is_file($imgPATH)) {
-        $hinh = "<img src='" . $imgPATH . "' height='100px',width='100px'";
+        $hinh = "<img src='" . $imgPATH . "' height='100px' width='100px'>";
 } else {
-        $hinh = "khum có ảnh";
+        $hinh = "không có ảnh";
 }
 ?>
+<link rel="stylesheet" href="../CSS/admin_product_update.css">
 <div class="row">
         <div class="row frmtitle">
                 <h1>CẬP NHẬT LOẠI HÀNG</h1>
         </div>
         <div class="row frmcontent">
-                <form action="index.php?action=updatesp" method="post" enctype="multipart/form-data">
+                <form action="index.php?action=updatesp" method="post" enctype="multipart/form-data"
+                        onsubmit="return redirectToPage()">
                         <div class="">
                                 <select name="iddm" id="">
                                         <option value="0" selected>Tất cả</option>
                                         <?php
                                         foreach ($listdm as $danhmuc) {
                                                 extract($danhmuc);
-                                                if ($IDDM = $IDDM)
+                                                if ($IDDM == $iddm)
                                                         $s = "selected";
                                                 else
                                                         $s = "";
@@ -43,8 +45,10 @@ if (is_file($imgPATH)) {
                                 <input type="text" name="soluong" id="rowlist" value="<?= $TONKHO ?>">
                         </div>
                         <div class="">Hình: <br>
-                                <input type="file" name="hinh" id="rowlist"><br><br>
-                                <?= $hinh ?>
+                                <input type="file" name="hinh" id="hinh" onchange="loadImage(this)"><br><br>
+                                <div id="imageContainer">
+                                        <?= $hinh ?>
+                                </div>
                         </div>
                         <div class="">Mô Tả:<br>
                                 <textarea name="mota" id="" cols="30" rows="10"><?= $MOTA ?></textarea>
@@ -52,9 +56,27 @@ if (is_file($imgPATH)) {
                         <div>
                                 <input type="hidden" name="id" value="<?= $MASP ?>">
                                 <input type="submit" name="capnhatsp" value="CẬP NHẬT">
-                                <input type="reset" value="Nhập lại">
-                                <a href="index.php?action=listsp"><input type="button" value="Danh Sách">
+                                <a href="index.php?action=listsp"><input type="button" value="Danh Sách"></a>
                         </div>
                 </form>
+
+                <script>
+                        // Function to load selected image into the image container
+                        function loadImage(input) {
+                                if (input.files && input.files[0]) {
+                                        var reader = new FileReader();
+                                        reader.onload = function (e) {
+                                                document.getElementById('imageContainer').innerHTML = '<img src="' + e.target.result + '" height="100px" width="100px">';
+                                        }
+                                        reader.readAsDataURL(input.files[0]);
+                                }
+                        }
+
+                        // Function to redirect to the listsp page after form submission
+                        function redirectToPage() {
+                                alert('Đã cập nhật thông tin sản phẩm thành công');
+                                // window.location.href = 'index.php?action=listsp';
+                                return true; // Allow form submission
+                        }
+                </script>
         </div>
-</div>
