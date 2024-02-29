86be0d0eeb2d1e55e63d6c3dbecea79d5ac708e5
Andrew0456 <phandat452006@gmail.com>
2024-02-29 22:01:56 +0700

    hi

---
+++ view/content/news.php
@@ -0,0 +1,20 @@
+<section id="main">
+    <?php  
+        foreach( $news as $tintuc )
+        {
+            extract($tintuc);
+            $tenbaiviet=$tintuc['tenbv'];
+            $tomtat=$tintuc['tomtat'];
+            $hinh=$tintuc['hinh'];
+            $idp=$tintuc['id'];
+            echo" <table class='noidung'>
+            <tr>
+              <th rowspan='2'> <a href='index.php?action=detailnews&idsp=$idp'><img src='../uploads/$hinh'alt='Sản phẩm 1'></a>
+              <h4>$tenbaiviet</h4> <text>$tomtat</text>
+              </a></th>
+            
+            </tr>
+          </table>";
+        }
+        ?>
+    </section>
