4ddc632361bb96ea4c07884fe440ac9c58362e90
Andrew0456 <phandat452006@gmail.com>
2024-03-02 00:33:00 +0700

    Hi

--- view/product/catalogs.php
+++ view/product/catalogs.php
@@ -1,50 +1,56 @@
 <link rel="stylesheet" href="../CSS/catalogs.css">
 <section id="main">
     <div class="container">
-        <h2><?php echo $loaisp1 ?></h2>
+        <h2>
+            <?php echo $loaisp1 ?>
+        </h2>
         <table class="noidung">
-     <tr>
-        <?php 
-        foreach( $list1 as $first )
-        {
-            extract($first);
-            $hinh = $first['hinhanh'];
-            $MASP = $first['MASP'];
-            $TENSP = $first['TENSP'];
-            $GIASP = $first['GIASP'];
-            echo "
+            <tr>
+                <?php
+                foreach ($list1 as $first) {
+                    extract($first);
+                    $hinh = $first['hinhanh'];
+                    $MASP = $first['MASP'];
+                    $TENSP = $first['TENSP'];
+                    $GIASP = $first['GIASP'];
+                    echo "
             <th rowspan='2'> 
-            <a href='index.php?action=showdetail&idsp=$MASP'><img height='100px' width='100px' src='../uploads/$hinh' alt='Sản phẩm 1'></a>
-             <h4>$TENSP</h4>
-             <p>Giá: $GIASP VND</p>
-             <button>Mua ngay</button>
+            <a href='index.php?action=showdetail&idsp=$MASP'><img class='img_logo_sp' src='../uploads/$hinh' alt='Sản phẩm 1'></a>
+            <h4 class='tensp'>$TENSP</h4>
+            <div class='price_button'>
+            <p class='price'>$GIASP VNĐ</p>
+            <button class='buy-button'>Mua ngay</button>
+            </div>
          </th>";
-        }
-        ?>
-        </tr>
+                }
+                ?>
+            </tr>
         </table>
 
-        <h2><?php echo $loaisp2 ?></h2>
+        <h2>
+            <?php echo $loaisp2 ?>
+        </h2>
         <table class="noidung">
-     <tr>
-        <?php 
-        foreach( $list2 as $second )
-        {
-            extract($second);
-            $hinh = $second['hinhanh'];
-            $MASP = $second['MASP'];
-            $TENSP = $second['TENSP'];
-            $GIASP = $second['GIASP'];
-            echo "
-            <th > 
-            <a href='index.php?action=showdetail&idsp=$MASP'><img height='100px' width='100px' src='../uploads/$hinh' alt='Sản phẩm 1'></a>
-             <h4>$TENSP</h4>
-             <p>Giá: $GIASP VND</p>
-             <button>Mua ngay</button>
+            <tr>
+                <?php
+                foreach ($list2 as $second) {
+                    extract($second);
+                    $hinh = $second['hinhanh'];
+                    $MASP = $second['MASP'];
+                    $TENSP = $second['TENSP'];
+                    $GIASP = $second['GIASP'];
+                    echo "
+                <th rowspan='2'> 
+                <a href='index.php?action=showdetail&idsp=$MASP'><img class='img_logo_sp' src='../uploads/$hinh' alt='Sản phẩm 1'></a>
+                <h4 class='tensp'>$TENSP</h4>
+                <div class='price_button'>
+                <p class='price'>$GIASP VNĐ</p>
+                <button class='buy-button'>Mua ngay</button>
+                </div>
          </th>";
-        }        
-        ?>  
-        </tr>
+                }
+                ?>
+            </tr>
         </table>
     </div>
 </section>
--- view/product/product.php
+++ view/product/product.php
@@ -1,18 +1,7 @@
-<link rel="stylesheet" href="../CSS/style.css">
+<link rel="stylesheet" href="../CSS/products.css">
 <section id="main">
     <div class="container" id="'.$LOAISP.'">
         <?php
-
-        foreach ($dmf as $dm) {
-            $THUTU = $dm['THUTU']; // Trích xuất giá trị của $THUTU từ mỗi phần tử trong mảng
-            $LOAISP = $dm['LOAISP'];
-
-            echo '<h2>' . $LOAISP . '</h2>';
-        }
-        // <table class="noidung">
-        //     <tr>
-        // <?php  
-        
         foreach ($dmf as $dm) {
             $THUTU = $dm['THUTU']; // Trích xuất giá trị của $THUTU từ mỗi phần tử trong mảng
             $LOAISP = $dm['LOAISP'];
@@ -20,16 +9,16 @@
             echo '<h2>' . $LOAISP . '</h2>';
         }
         ?>
-            <?php
-            $imgPath="uploads/";
-                foreach ($dssp as $sp) {
-                    extract($sp);
-                    $hinh = $imgPath . $hinhanh;
-                    echo'   <th> 
-                    <a href="index.php?action=showdetail&idsp='.$MASP.'">
-                                <img  src="'.$hinh.'" alt="'.$TENSP.'" height="100px" width="100px">
-                                <h4>'.$TENSP.'</h4>
-                                <p>'. $GIASP . " ₫ ".'</p>
+        <?php
+        $imgPath = "uploads/";
+        foreach ($dssp as $sp) {
+            extract($sp);
+            $hinh = $imgPath . $hinhanh;
+            echo '   <th> 
+                    <a href="index.php?action=showdetail&idsp=' . $MASP . '">
+                                <img  src="' . $hinh . '" alt="' . $TENSP . '" height="100px" width="100px">
+                                <h4>' . $TENSP . '</h4>
+                                <p>' . $GIASP . " ₫ " . '</p> 
                                 <button>Mua ngay</button>
                         </a>
                     </th>
