df7b62047e2ae448a234bac5e3efd58ffda31e21
Andrew0456 <phandat452006@gmail.com>
2024-03-02 09:14:06 +0700

    Hi

--- view/product/product.php
+++ view/product/product.php
@@ -1,31 +1,32 @@
 <link rel="stylesheet" href="../CSS/catalogs.css">
 <section id="main">
     <div class="container" id="'.$LOAISP.'">
-        <?php
-        foreach ($dmf as $dm) {
-            $THUTU = $dm['THUTU']; // Trích xuất giá trị của $THUTU từ mỗi phần tử trong mảng
-            $LOAISP = $dm['LOAISP'];
+        <table class="noidung">
+            <tr>
+                <?php
+                foreach ($dmf as $dm) {
+                    $THUTU = $dm['THUTU']; // Trích xuất giá trị của $THUTU từ mỗi phần tử trong mảng
+                    $LOAISP = $dm['LOAISP'];
 
-            echo '<h2>' . $LOAISP . '</h2>';
-        }
-        ?>
-        <?php
-        $imgPath = "uploads/";
-        foreach ($dssp as $sp) {
-            extract($sp);
-            $hinh = $imgPath . $hinhanh;
-            echo '   <th rowspan="2"> 
-                    <a href="index.php?action=showdetail&idsp=' . $MASP . '">
-                        <img  src="' . $hinh . '" alt="' . $TENSP . '" height="100px" width="100px">
-                        <h4 class="tensp">' . $TENSP . '</h4>
-                        <p class="price">' . $GIASP . " ₫ " . '</p> 
-                        <button class="buy-button">Mua ngay</button>
-                    </a>
-                    </th>
-                    ';
-        }
-        ?>
-        </tr>
+                    echo '<h2>' . $LOAISP . '</h2>';
+                }
+                ?>
+                <?php
+                $imgPath = "uploads/";
+                foreach ($dssp as $sp) {
+                    extract($sp);
+                    $hinh = $imgPath . $hinhanh;
+                    echo '<th rowspan="2"> 
+                        <a href="index.php?action=showdetail&idsp=' . $MASP . '">
+                            <img  src="' . $hinh . '" alt="' . $TENSP . '">
+                            <h4 class="tensp">' . $TENSP . '</h4>
+                            <p class="price">' . $GIASP . " VNĐ " . '</p> 
+                            <button class="buy-button">Mua ngay</button>
+                        </a>
+                    </th>';
+                }
+                ?>
+            </tr>
         </table>
     </div>
     </div>
